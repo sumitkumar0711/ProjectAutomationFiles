63ac467fdcdf2fd7d179d89b822c57b51a497e8a
SUMIT KUMAR <kumarsumit4961@gmail.com>
2019-09-04 00:11:08 +0530

    This shuts down the system of the user by slowly moving the curser in front of the user.

---
+++ ShutDownInFrontOfTheUser.py
@@ -0,0 +1,8 @@
+import pyautogui
+pyautogui.FAILSAFE=False
+pyautogui.moveTo(0,1080,3)
+pyautogui.click(0,1080)
+pyautogui.moveTo(20,1000,2)
+pyautogui.click(20,1000)
+pyautogui.moveTo(20,900,1)
+pyautogui.click(20,900)
